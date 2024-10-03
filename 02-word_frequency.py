ee31a87514dfb860a2a2b08f27eeeedaf12d064b
anya-decarlo <182010635+anya-decarlo@users.noreply.github.com>
2024-10-02 18:09:55 -0700

    commit word frequency

--- 02-word_frequency.py
+++ 02-word_frequency.py
@@ -24,10 +24,10 @@ def word_frequency(text):
 
     # Your code here
     words = text.lower().split()
-    for word in words 
+    for word in words:
         word = word.strip(". , ! : ;() [] \ ""  ")
         if word in frequencies: 
-            frequencies[word] + = 1
+            frequencies[word] += 1
         else: 
             frequencies[word] = 1
     
